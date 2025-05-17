27528737d8f390ee7a7232280aa121d9a4affe4f
Maximitas <Lassetimm@outlook.dk>
2025-05-17 20:57:52 +0200

    ...

--- src/router/Games.jsx
+++ src/router/Games.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import styles from './App.module.css';
+import styles from '../App.module.css';
 
 const Games = () => {
   const [games, setGames] = useState([]);
